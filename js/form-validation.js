9aa3d189210d71587b9ebca6bb4cfcdb337ca670
Sanil Shakya <sanil@gurzu.net>
2021-08-24 13:50:55 +0545

    added danger-text to form validation

--- js/form-validation.js
+++ js/form-validation.js
@@ -12,19 +12,21 @@ submit.addEventListener('click', (e) => {
         e.preventDefault();
         let newP = document.createElement("p");
         newP.innerText = 'name should be longer than 3 characters';
-        newP.className = 'danger-text'
+        newP.className = 'danger-text';
         label[0].appendChild(newP);
     };
     if(!(/\w+[@][a-z]+[.][a-z]+/ig).test(email.value)){
         e.preventDefault();
         let newP = document.createElement("p");
         newP.innerText = 'invalid email';
+        newP.className = 'danger-text';
         label[1].appendChild(newP);
     }
     if(message.value.length < 3){
         e.preventDefault();
         let newP = document.createElement("p");
         newP.innerText = 'name should be longer than 3 characters';
+        newP.className = 'danger-text';
         label[2].appendChild(newP);
     }
 })
